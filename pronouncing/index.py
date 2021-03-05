f7a22fca5eeeb197b76d3007dc1f4ab7b5c73a75
nvkhuong <vankhuong240499@gmail.com>
2021-03-05 16:47:09 +0700

    push

--- pronouncing/index.py
+++
@@ -1,4 +0,0 @@
-import pronouncing
-# phones = pronouncing.phones_for_word("sighs")[0]
-print(pronouncing.phones_for_word("pronouncing"))
-# print([u'incise', u'incised', u'incisor', u'incisors', u'malloseismic'])
